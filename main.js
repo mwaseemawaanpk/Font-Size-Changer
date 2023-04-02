f021c2f63d96085e279895229f35211c4343bbea
Muhammad <waseemawanpk@gmail.com>
2023-04-03 00:18:12 +0500

    Font-size-changer

---
+++ main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#range-slider").on("input change", function(){
+        $(".text").css("font-size", $(this).val() + "px");
+
+        $(".value").text($(this).val() + "px");
+    });
+});
